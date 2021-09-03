18c40397ab960a9478e97516c577652b2ba61fe9
NA-Siddiky <siddiky.academic@gmail.com>
2021-09-03 20:01:06 +0600

    adding weither

--- JS/app.js
+++ JS/app.js
@@ -10,7 +10,16 @@ const searchTemperature = () => {
         .then(response => response.json())
         .then(data => displayTemperature(data))
 }
-
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
 const displayTemperature = temperature => {
     console.log(temperature)
+    setInnerText('city', temperature.name)
+    setInnerText('temperature', temperature.main.temp)
+    setInnerText('condition', temperature.weather[0].main)
+
+    const url = `https://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`
+    const imgIcon = document.getElementById("weather-icon");
+    imgIcon.setAttribute("src", url);
 }
